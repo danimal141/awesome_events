e92b26eb6d6e476bd7b680b57422c75075a28a54
danimal141 <hideaki.ishii1204@gmail.com>
2014-07-26 15:22:08 +0900

    Introduce kaminari for pagination

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+  PER = 3
+
   def index
+    @events = Event.all.page(params[:page]).per(PER)
+      .where('start_time > ?', Time.zone.now).order(:start_time)
   end
 end
