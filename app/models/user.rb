543e5598ae6a6117e919a7181307c628b930f476
danimal141 <hideaki.ishii1204@gmail.com>
2014-07-29 07:47:44 +0900

    Limit search condition

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,17 @@ class User < ActiveRecord::Base
   has_many :tickets
   has_many :participating_events, through: :tickets, source: :event
 
-  def self.find_or_create_from_auth_hash auth_hash
-    provider = auth_hash[:provider]
-    uid = auth_hash[:uid]
-    nickname = auth_hash[:info][:nickname]
-    image_url = auth_hash[:info][:image]
+  class << self
+    def find_or_create_from_auth_hash auth_hash
+      provider = auth_hash[:provider]
+      uid = auth_hash[:uid]
+      nickname = auth_hash[:info][:nickname]
+      image_url = auth_hash[:info][:image]
 
-    User.find_or_create_by(provider: provider, uid: uid) do |user|
-      user.nickname = nickname
-      user.image_url = image_url
+      User.find_or_create_by(provider: provider, uid: uid) do |user|
+        user.nickname = nickname
+        user.image_url = image_url
+      end
     end
   end
 
